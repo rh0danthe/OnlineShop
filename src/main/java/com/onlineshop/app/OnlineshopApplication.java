4402898ca027a0826e2a47a7d45a0cda9c5aa81f
rh0danthe <rita.pichushkina.04@mail.ru>
2024-06-30 18:58:17 +0300

    first commit

---
+++ src/main/java/com/onlineshop/app/OnlineshopApplication.java
@@ -0,0 +1,14 @@
+package com.onlineshop.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class OnlineshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineshopApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/onlineshop/app/controllers/TestController.java
@@ -0,0 +1,16 @@
+package com.onlineshop.app.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping
+    public HttpStatus Test()
+    {
+        return HttpStatus.OK;
+    }
+}

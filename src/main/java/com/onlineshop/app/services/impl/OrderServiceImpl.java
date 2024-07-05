7bce2f4f4d6193c380ebde12da7ae5f64a0cce51
rh0danthe <rita.pichushkina.04@mail.ru>
2024-07-05 04:34:55 +0300

    logger added

--- src/main/java/com/onlineshop/app/services/impl/OrderServiceImpl.java
+++ src/main/java/com/onlineshop/app/services/impl/OrderServiceImpl.java
@@ -10,6 +10,8 @@ import com.onlineshop.app.repository.CustomerRepository;
 import com.onlineshop.app.repository.OrderRepository;
 import com.onlineshop.app.repository.ProductRepository;
 import com.onlineshop.app.services.OrderService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,7 @@ import java.util.Optional;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+    private static final Logger logger = LogManager.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final CustomerRepository customerRepository;
     private final ProductRepository productRepository;
@@ -35,8 +38,11 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public OrderResponse create(OrderRequest request) throws IOException {
         Optional<Customer> customerOpt = customerRepository.findById(request.getCustomerId());
+
         if (customerOpt.isEmpty()) {
-            throw new IOException(String.format("Покупателя с айди %s не существует", request.getCustomerId()));
+            String errorMessage = String.format("Покупателя с айди %s не существует", request.getCustomerId());
+            logger.error("Ошибка при создании заказа: {}", errorMessage);
+            throw new IOException(errorMessage);
         }
 
         List<Product> products = new ArrayList<>();
@@ -44,7 +50,9 @@ public class OrderServiceImpl implements OrderService {
         for (int productId : request.getProductIds()){
             Optional<Product> productOpt = productRepository.findById(productId);
             if (productOpt.isEmpty()) {
-                throw new IOException(String.format("Продукта с айди %s не существует", productId));
+                String errorMessage = String.format("Продукта с айди %s не существует", productId);
+                logger.error("Ошибка при создании заказа: {}", errorMessage);
+                throw new IOException(errorMessage);
             }
             else products.add(productOpt.get());
         }
@@ -52,6 +60,9 @@ public class OrderServiceImpl implements OrderService {
         Order order = new Order(customerOpt.get(), products);
 
         Order savedOrder = orderRepository.save(order);
+
+        logger.info("Заказ успешно создан, айди: {}", order.getId());
+
         return OrderMapper.INSTANCE.orderToResponse(savedOrder);
     }
 }
